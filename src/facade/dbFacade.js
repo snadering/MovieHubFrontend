5e9c735c2efd823f0d078e9d5f91ecc5fc04bc43
TobiasTheDanish <t.hc199811@gmail.com>
2023-12-11 22:32:51 +0100

    Added dbFacade, for login and signup

---
+++ src/facade/dbFacade.js
@@ -0,0 +1,41 @@
+export default function dbFacade() {
+  const getUser = (form) => {
+    return {
+      username: form.querySelector("[name='username']").value,
+      password: form.querySelector("[name='password']").value,
+    };
+  }
+
+  const login = async (user) => {
+    return await fetch("http://localhost:7070/api/v1/auth/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+      body: JSON.stringify(user),
+    }).then((res) => res.json());
+  };
+
+  const register = async (user) => {
+    return await fetch("http://localhost:7070/api/v1/auth/register", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+      body: JSON.stringify(user),
+    }).then((res) => res.json());
+  };
+
+  const setToken = (token) => {
+    localStorage.setItem("jwtToken", token);
+  }
+
+  return {
+    getUser,
+    login,
+    register,
+    setToken,
+  }
+}
