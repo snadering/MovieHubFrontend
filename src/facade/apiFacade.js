aa20f8ffff5e4edae7a0f0b83a9fc6eb1514e531
snadering <cph-sr319@cphbusiness.dk>
2023-12-12 18:02:51 +0100

    api key and env file added

--- src/facade/apiFacade.js
+++ src/facade/apiFacade.js
@@ -1,11 +1,12 @@
 const apiFacade = () => {
 
+    const API_KEY = process.env.API_KEY;
     
     const options = {
         method: 'GET',
         headers: {
             accept: 'application/json',
-            Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3NTA4NDE1YTNlZmQyMmI3N2VkNmM3ODc0MzI1MDJlYiIsInN1YiI6IjY1MGI1NDJmY2FkYjZiMDBhYmM2YjY3NyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.3bgpWgIBh-7eCFlLg-QJ1Kh4uvvYBOpTaoQlplWCQdg'
+            Authorization: `Bearer ${API_KEY}`
         }
     };
 
@@ -18,15 +19,6 @@ const apiFacade = () => {
             throw new Error(error);
         }
     }
-
-    /*
-    const getAllMovies = async () => {
-        return await fetch('https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc', options)
-            .then(response => response.json())
-            .then(response => console.log(response))
-            .catch(err => console.error(err));
-    }
-    */
 
     return {
         getAllMovies,
