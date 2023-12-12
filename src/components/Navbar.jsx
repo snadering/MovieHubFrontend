4ff7f7c3fdffe2daba7c48d465ce7dc178b6f2fc
ChristianStennicke <113089191+ChristianStennicke@users.noreply.github.com>
2023-12-12 17:33:22 +0100

    Your commit message

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,32 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+
 const Navbar = () => {
-  return(
-    <>
-        <h1>Navbar</h1>
-    </>
+  return (
+    <nav className="bg-gray-800 p-4">
+      <ul className="flex justify-end space-x-4 text-white">
+        <li>
+          <NavLink to="/movies" activeClassName="font-bold">
+            All Movies
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/top" activeClassName="font-bold">
+            Top Movies
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/login" activeClassName="font-bold">
+            Login
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/signup" activeClassName="font-bold">
+            Signup
+          </NavLink>
+        </li>
+      </ul>
+    </nav>
   );
 };
 
