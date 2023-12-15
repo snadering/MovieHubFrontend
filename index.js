c457ae765ba523ce4e68954ff1d728920917e4d6
TobiasTheDanish <t.hc199811@gmail.com>
2023-12-15 10:22:00 +0100

    Added SearchBar component, and search endpoint

--- index.js
+++ index.js
@@ -12,42 +12,62 @@ const API_KEY = process.env.REACT_APP_API_KEY;
 
 app.use(cors())
 
-app.get('/movies', (req, res) => {
-    const options = {
-        method: 'GET',
-        url: 'https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc',
-        headers: {
-            accept: 'application/json',
-            Authorization: `Bearer ${API_KEY}`
-        }
-    };
+app.get("/movies", (req, res) => {
+  const options = {
+    method: "GET",
+    url: "https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc",
+    headers: {
+      accept: "application/json",
+      Authorization: `Bearer ${API_KEY}`,
+    },
+  };
 
-    axios.request(options)
-        .then((response) => {
-            res.json(response.data)
-        }).catch((error) => {
-            console.error(error)
-        })
-   
-})
+  axios
+    .request(options)
+    .then((response) => {
+      res.json(response.data);
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+});
 
-app.get('/movies/:id', (req, res) => {
-    const options = {
-        method: 'GET',
-        url: `https://api.themoviedb.org/3/movie/${req.params.id}?language=en-US`,
-        headers: {
-            accept: 'application/json',
-            Authorization: `Bearer ${API_KEY}`
-        }
-    };
+app.get("/movies/:id", (req, res) => {
+  const options = {
+    method: "GET",
+    url: `https://api.themoviedb.org/3/movie/${req.params.id}?language=en-US`,
+    headers: {
+      accept: "application/json",
+      Authorization: `Bearer ${API_KEY}`,
+    },
+  };
 
-    axios.request(options)
-        .then((response) => {
-            res.json(response.data)
-        }).catch((error) => {
-            console.error(error)
-        })
-})
+  axios
+    .request(options)
+    .then((response) => {
+      res.json(response.data);
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+});
+
+app.get("/search/movies", (req, res) => {
+
+  const options = {
+    method: "GET",
+    url: `https://api.themoviedb.org/3/search/movie?query=${req.query.query}&include_adult=false&language=en-US&page=1`,
+    headers: {
+      accept: "application/json",
+      Authorization: `Bearer ${API_KEY}`,
+    },
+  };
+
+  axios
+    .request(options)
+    .then((response) => res.json(response.data))
+    .catch((err) => console.error(err));
+});
 
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -3,6 +3,7 @@ import getApiFacade from "../facade/apiFacade";
 import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import { Audio } from "react-loader-spinner";
+import SearchBar from "./SearchBar";
 
 const MovieList = () => {
   const [movies, setMovies] = useState([]);
@@ -22,31 +23,30 @@ const MovieList = () => {
 
   return (
     <>
-      {Object.keys(movies).length === 0 ? (
-        <div className="flex justify-center w-full">
-          <Audio
-            height="200"
-            width="200"
-            radius="9"
-            color="orange"
-            ariaLabel="loading"
-            wrapperStyle=""
-            wrapperClass=""
-          />
-        </div>
-      ) : (
-        <>
-          <div className="container mx-auto p-8">
-            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8">
-              {movies.map((movie, index) => (
-                <Link to={`/movies/${movie.id}`} key={movie.id}>
-                  <MovieCard {...movie} />
-                </Link>
-              ))}
-            </div>
+      <div className="container mx-auto p-8 space-y-8">
+        <SearchBar setMovies={setMovies} />
+        {Object.keys(movies).length === 0 ? (
+          <div className="flex justify-center w-full">
+            <Audio
+              height="200"
+              width="200"
+              radius="9"
+              color="orange"
+              ariaLabel="loading"
+              wrapperStyle=""
+              wrapperClass=""
+            />
           </div>
-        </>
-      )}
+        ) : (
+          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8">
+            {movies.map((movie, index) => (
+              <Link to={`/movies/${movie.id}`} key={movie.id}>
+                <MovieCard {...movie} />
+              </Link>
+            ))}
+          </div>
+        )}
+      </div>
     </>
   );
 };
