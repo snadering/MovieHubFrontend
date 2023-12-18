a53cf5012a62b5c4e791afbd572b7ecaf4d5dc1a
TobiasTheDanish <t.hc199811@gmail.com>
2023-12-18 18:57:01 +0100

    Fix navigation warning on Logout component

--- src/components/Logout.jsx
+++ src/components/Logout.jsx
@@ -1,12 +1,22 @@
 import { useNavigate } from "react-router-dom";
 import dbFacade from "../facade/dbFacade";
+import { useEffect } from "react";
 
 function Logout() {
   const navigate = useNavigate();
-  const token = dbFacade().getToken();
 
-  dbFacade().logout(token);
-  navigate("/login");
+  useEffect(() => {
+    const token = dbFacade().getToken();
+
+    dbFacade().logout(token);
+    navigate("/login");
+  }, []);
+
+  return (
+    <>
+      <p>Logout</p>
+    </>
+  );
 }
 
 export default Logout;
